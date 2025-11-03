c29b5e81043ecda9c9ffe2d15aecc0999df9ab4d
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2025-11-03 18:44:10 +0530

    dotenv

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose =require('mongoose')
 
 //const mongoURL ='mongodb://localhost:27017/hotels'
- //const mongoURL ='mongodb+srv://ambekarshraddha21_db_user:ska2001@cluster0.oteku1r.mongodb.net/'
+//const mongoURL ='mongodb+srv://ambekarshraddha21_db_user:ska2001@cluster0.oteku1r.mongodb.net/'
 
- const mongoURL =process.env.MONGODB_URL
+const mongoURL =process.env.MONGODB_URL
 mongoose.connect(mongoURL)
 
 const db =mongoose.connection;
